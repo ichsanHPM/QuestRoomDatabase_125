255ede11d08a418903e1c20bcdeddb6ee2738a4d
ichsanHPM <ichsanhpm@gmail.com>
2024-12-16 06:54:43 +0700

    Menambahkan fungsi suspend untuk menghapus dan memperbaharui data mahasiswa

--- app/src/main/java/com/example/monitoringapplication/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/monitoringapplication/repository/RepositoryMhs.kt
@@ -9,6 +9,12 @@ interface RepositoryMhs {
     //suspend digunakan untuk operasi yang berat seperti create, delete, insert
     suspend fun insertMhs(mahasiswa: Mahasiswa)
 
+    //untuk menghapus data mahasiswa
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    //untuk memperbaharui data mahasiswa
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
+
     //metode ini memanggil fungsi get all mahasiswa dari MahasiswaDao untuk mendapatkan semua data
     fun getAllMahasiswa(): Flow<List<Mahasiswa>>
 
