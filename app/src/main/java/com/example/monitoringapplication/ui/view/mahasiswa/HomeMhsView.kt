90e277aa0aa27174a5d46c29b166aeba48d9604b
ichsanHPM <ichsanhpm@gmail.com>
2024-12-16 11:54:08 +0700

    Menambahkan HomeMhsView dengan Scaffold, TopAppBar, dan FloatingActionButton untuk menampilkan daftar Mahasiswa

--- app/src/main/java/com/example/monitoringapplication/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/monitoringapplication/ui/view/mahasiswa/HomeMhsView.kt
@@ -9,17 +9,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+
+
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -27,9 +35,58 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.monitoringapplication.data.entity.Mahasiswa
+import com.example.monitoringapplication.ui.costumwidget.TopAppBar
+import com.example.monitoringapplication.ui.viewmodel.HomeMhsViewModel
+
 import com.example.monitoringapplication.ui.viewmodel.HomeUiState
+import com.example.monitoringapplication.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.launch
+
+
+@Composable
+fun HomeMhsView(
+    viewModel: HomeMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddMhs: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Mahasiswa",
+                showBackButton = false,
+                onBack = { },
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddMhs,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Mahasiswa",
+                )
+            }
+        }
+    ) {
+            innerPadding ->
+        val homeUiState by viewModel.homeUiState.collectAsState()
+
+        BodyHomeMhsView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+
+}
 
 
 @Composable
@@ -93,7 +150,6 @@ fun BodyHomeMhsView(
     }
 }
 
-
 @Composable
 fun ListMahasiswa(
     listMhs: List<Mahasiswa>,
@@ -114,7 +170,6 @@ fun ListMahasiswa(
         )
     }
 }
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
