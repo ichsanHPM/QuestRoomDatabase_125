6ee1754044758f0db7f8b65869431a9da561de74
ichsanHPM <ichsanhpm@gmail.com>
2024-12-16 07:39:07 +0700

    Menambahkan data class DetailUiState untuk menampung data yang akan ditampilkan di UI

--- app/src/main/java/com/example/monitoringapplication/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/monitoringapplication/ui/viewmodel/DetailMhsViewModel.kt
@@ -2,6 +2,20 @@ package com.example.monitoringapplication.ui.viewmodel
 
 import com.example.monitoringapplication.data.entity.Mahasiswa
 
+// Data Class untuk menampung data yang akan ditampilkan di UI
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
 
 // Memindahkan data dari entity ke UI
 fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent {
