3a9a98c238ff869f8eb28eedaea2d16f0224b7cc
ichsanHPM <ichsanhpm@gmail.com>
2024-12-16 07:42:05 +0700

    Menambahkan DetailMhsViewModel untuk mengelola UI State detail Mahasiswa dan fungsi penghapusan data

--- app/src/main/java/com/example/monitoringapplication/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/monitoringapplication/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,6 +1,66 @@
 package com.example.monitoringapplication.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.monitoringapplication.data.entity.Mahasiswa
+import com.example.monitoringapplication.repository.RepositoryMhs
+import com.example.monitoringapplication.ui.navigation.AlamatNavigasi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+
+class DetailMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+) : ViewModel() {
+    private val _nim: String = checkNotNull(savedStateHandle[AlamatNavigasi.DestinasiDetail.NIM])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+
+    fun deleteMhs() {
+        detailUiState.value.detailUiEvent.toMahasiswaEntity().let {
+            viewModelScope.launch {
+                repositoryMhs.deleteMhs(it)
+            }
+        }
+    }
+}
+
 
 // Data Class untuk menampung data yang akan ditampilkan di UI
 data class DetailUiState(
