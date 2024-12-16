00de7f285e903323585f95d655eb556671215032
ichsanHPM <ichsanhpm@gmail.com>
2024-12-16 07:57:15 +0700

    Menambahkan inisialisasi ViewModel untuk HomeMhs, DetailMhs, dan UpdateMhs

--- app/src/main/java/com/example/monitoringapplication/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/monitoringapplication/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.monitoringapplication.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -11,6 +12,26 @@ object PenyediaViewModel{
         initializer {
             MahasiswaViewModel(
                 krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
             )
         }
     }
